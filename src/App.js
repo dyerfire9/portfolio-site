97ec112dfb0bf8eb9affc2445447a7fb3552ab9f
dyerfire9 <canadianraza@yahoo.com>
2022-12-08 13:37:35 -0500

    Initialized Components and created navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-
+import Navbar from './components/Navbar'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function App() {
   return (
     <div className="App">
-    
+      <Navbar/>
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,53 @@
+import React from "react"
+import "./style.css";
 
 export default function Navbar(){
 
-    return (
-        <div>
+    function navSlide() {
+        const burger = document.querySelector('.navburger')
+        const navbar = document.querySelector('.navbar-links')
+        const navLink = document.querySelectorAll('.nav-link')
+        
+        
+        navbar.classList.toggle('navbar-active')
+        burger.classList.toggle('toggle')
+        
+        navLink.forEach(button => button.addEventListener('click', closeMenu));
 
+        function closeMenu(){
+            burger.classList.remove('toggle')
+            navbar.classList.remove('navbar-active')
+        }
+    }
+
+    React.useEffect(() => {
+        // 👇️ scroll to top on page load
+        window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
+      }, []);
+
+    function toTop(){
+        window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
+    }
+
+    return (
+        <div className="header">
+            <nav className="navbar">
+                <div className="navbar-name" onclick={toTop}>Abdul Mannan</div>
+               
+                <ul className="navbar-links">
+                    <li><a href="#" className="nav-link hover-ani">Software</a></li>
+                    <li><a href="#" className="nav-link hover-ani">Timeline</a></li>
+                    <li><a href="#" className="nav-link hover-ani">Projects</a></li>
+                    <li><a href="#" className="nav-link hover-ani">Education</a></li>
+                    <li><a href="#" className="nav-link hover-ani">Contact</a></li>
+                </ul>
+        
+                <div className="navburger" onClick={navSlide}>
+                    <div className="burger-line1"></div>
+                    <div className="burger-line2"></div>
+                    <div className="burger-line3"></div>
+                </div>
+            </nav>
         </div>
     )
 }
