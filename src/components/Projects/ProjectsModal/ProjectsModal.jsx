7938260fc37a7a8e6bc70264e1c0634ac7322ba7
dyerfire9 <canadianraza@yahoo.com>
2022-12-15 01:04:44 -0500

    Added click anywhere to exit modal functionality, further testing may be required

--- src/components/Projects/ProjectsModal/ProjectsModal.jsx
+++ src/components/Projects/ProjectsModal/ProjectsModal.jsx
@@ -4,14 +4,22 @@ import { FaGithub} from 'react-icons/fa';
 import { RiCloseLine } from "react-icons/ri";
 
 export default function ProjectsModal(props){   
+    let modal = document.getElementById('projects-modal');
     const style1 = { color: "white", fontSize: "1.2em", display:'inline-flex', 'verticalAlign': 'text-top'}
     const style2 = { color:"white", fontSize: "1.5em"}
 
     function toggle(){
         props.changeState(prevVal => ({...prevVal, isOpen: !prevVal.isOpen}))
     }
+
+
+    window.onclick = function(event) {
+        if (event.target === modal) {
+            props.changeState(prevVal => ({...prevVal, isOpen: !prevVal.isOpen}))
+        }
+  }
     return(
-        <div className={`projects-modals ${props.state.isOpen ? 'modal-open' : 'modal-closed'}`}>
+        <div id='projects-modal' className={`projects-modal ${props.state.isOpen ? 'modal-open' : 'modal-closed'}`}>
             <div className="modal-container">
                 <div className="modal-header" onClick={toggle}> <button><RiCloseLine style={style2}/></button></div>
 
