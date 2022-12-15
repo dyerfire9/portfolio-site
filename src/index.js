3291e5aa0a35cf66afdfe35387128fc3a696284c
dyerfire9 <canadianraza@yahoo.com>
2022-12-15 01:16:09 -0500

    fixed ReactDOM.render bug

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import 'font-awesome/css/font-awesome.min.css';
 import App from './App';
 
-ReactDOM.render(<App/>, document.getElementById("root"));
-
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
