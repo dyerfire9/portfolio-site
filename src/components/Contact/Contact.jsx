c84361f0123fc4b37b5702cda60bd3930dde9a8a
dyerfire9 <canadianraza@yahoo.com>
2022-12-17 01:20:48 -0500

    added email copy functionality

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -6,14 +6,24 @@ import { MdEmail, MdContentCopy} from "react-icons/md";
 export default function Contact() {
     const style1 = {color: "#145DA0", fontSize: "1.5em", verticalAlign: "middle"}
 
-    function handleOver(){
+    function handleClick(){
+        const tag = document.getElementById('email-text')
+        const email = 'abdulmannancomp@gmail.com'
 
+        navigator.clipboard.writeText(email)
+        tag.textContent='Copied'
+        setTimeout(() => {tag.textContent='Copy Email'}, 3000);
     }
+    
     return (
         <div className="contact">
             <h2 className="contact-title">Contact</h2>
             <div className='contact-container'>
-                <a className="contact-email"  ><MdEmail style={style1}/> Copy Email <MdContentCopy style={style1}/></a>
+                <a className="contact-email" onClick={handleClick}>
+                    <MdEmail style={style1}/> 
+                    <p id="email-text">Copy Email</p>
+                    <MdContentCopy style={style1}/>
+                </a>
                 <a className="contact-linkedin" href="https://linkedin.com/in/abdulmannancomp" target="_blank"><BsLinkedin style={style1}/> Linkedin <BsArrowUpRightSquare style={style1}/></a>
                 <a className="contact-github" href="https://github.com/dyerfire9" target="_blank">< BsGithub style={style1}/> GitHub <BsArrowUpRightSquare style={style1}/></a>
             </div>
