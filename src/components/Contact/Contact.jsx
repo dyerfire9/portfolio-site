3f090c5d63cb73ccb10486dfe8a4728e60a801ed
dyerfire9 <canadianraza@yahoo.com>
2022-12-17 00:41:51 -0500

    added button links

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -14,8 +14,8 @@ export default function Contact() {
             <h2 className="contact-title">Contact</h2>
             <div className='contact-container'>
                 <a className="contact-email"  ><MdEmail style={style1}/> Copy Email <MdContentCopy style={style1}/></a>
-                <a className="contact-linkedin" href="www.linkedin.com/in/abdulmannancomp"><BsLinkedin style={style1}/> Linkedin <BsArrowUpRightSquare style={style1}/></a>
-                <a className="contact-github" href="https://www.w3schools.com">< BsGithub style={style1}/> GitHub <BsArrowUpRightSquare style={style1}/></a>
+                <a className="contact-linkedin" href="https://linkedin.com/in/abdulmannancomp" target="_blank"><BsLinkedin style={style1}/> Linkedin <BsArrowUpRightSquare style={style1}/></a>
+                <a className="contact-github" href="https://github.com/dyerfire9" target="_blank">< BsGithub style={style1}/> GitHub <BsArrowUpRightSquare style={style1}/></a>
             </div>
         </div>
     )
