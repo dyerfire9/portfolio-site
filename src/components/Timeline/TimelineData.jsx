dffaa787ec5e5c8214a1091ac9ce1ef1ecbce4a1
dyerfire9 <canadianraza@yahoo.com>
2023-03-10 22:57:44 -0500

    updated timeline

--- src/components/Timeline/TimelineData.jsx
+++ src/components/Timeline/TimelineData.jsx
@@ -1,12 +1,22 @@
 export default [
+     {
+        position: 'Front End Developer',
+        company: 'National Society of Black Engineers (NSBE)',
+        time: 'December 2022 - February 2023',
+        isExpanded: true,
+        descriptionItems: ["Developed and maintained a promotional website using Javascript, HTML, and CSS while following Agile methods to ensure efficiency and user satisfaction",
+                           "Collaborated with design and marketing teams to enhance web design and incorporate their designs which improved direct web traffic by <b>20%</b>",
+                           "Spearheaded an efficient SEO campaign resulting in a <b>60%</b> organic web traffic increase and <b>top 3 web rank</b> through leveraging keyword analysis and Google Analytics",
+                           "Conducted code reviews with peers and implemented debugging solutions which optimized workflow for efficient development and higher quality code"]
+    },
     {
         position: 'Web Application Developer',
         company: 'Self Employed',
         time: 'April 2022 - Present',
-        isExpanded: true,
+        isExpanded: false,
         descriptionItems: ["Followed the software design principles to create & maintain several web applications which <b>improved workflow by 75%</b>",
-                            "Developed 4 web apps which help users be more efficient and effective with their work. For example, built a <b>Leads & Reference Tracker</b> - with currently <b>over 150 users</b> - which allows one to save and track useful links one may need for a report or essay",
-                            "Further optimized the web applications such as Chrome Extensions, which are just a click away and <b>improved work efficiency by 90%</b>",
+                            "Developed 4 web apps which help users be more efficient and effective with their work. For example, built a <b>Leads & Reference Tracker</b> - with currently <b>over 250 users</b> - which allows one to save and track useful links one may need for a report or essay",
+                            "Further optimized the web applications such as Chrome Extensions, which are just a click away and <b>improved work efficiency by 70%</b>",
                             "Other interesting web apps include: <b>Handy Calculator, Currency Converter, ViewMars</b>",
                             "Total Downloads from all apps are: <b>500+</b>"
                          ]
@@ -23,24 +33,13 @@ export default [
                            "Experience with HTML, CSS, Javascript, React framework, Bootstrap, Adobe (Photoshop & InDesign)"]
     },
     {
-        position: 'Web & Graphic Designer',
-        company: 'False Flags (E-sports Team)',
-        time: 'January 2020 - November 2020',
-        isExpanded: false,
-        descriptionItems: ["Worked on and maintained the team's website and resolved issues related to device <b>compatibility</b> and <b>optimization</b>",
-                           "Conducted frequent optimizations on the website for a dynamic experience & <b>quicker load times</b>",
-                           "Conceptualised and applied website design ideas that <b>improved UX</b> and ensured <b>easy navigation</b>",
-                           "Designed & integrated digital media using Adobe Suite and planned outreach events which <b>increased web traffic by 40%</b>",
-                           "Experience with HTML, CSS, Javascript, Vue.js framework, Adobe (Photoshop & InDesign)"]
-    },
-    {
         position: 'Web Development & Product Management Intern',
         company: 'Property Spark',
         time: 'Sept 2017 – Feb 2018',
         isExpanded: false,
-        descriptionItems: ["Researched & created over 35 blogs which were read by (on average) <b>4000+</b> individuals shortly after publishing",
-                           "Researched and Implemented SEO techniques on blogs which <b>enhanced traffic by 20%</b>",
-                           "Completed over 50 research projects on realtors and brokerages, properties which <b>increased customer base by over 30%</b>",
-                           "Published a blog that got the <b>most hits in one day, in company history</b>"]
+        descriptionItems: ["Created over <b>35</b> blogs using JavaScript, HTML, and CSS that were read by (on average) <b>4000+</b> individuals shortly after publishing, improving lead conversion by <b>10%</b>",
+                           "Conducted over <b>50</b> market research projects in order to identify potential markets and leads which increased the overall customer base by over <b>30%</b>",
+                           "Developed landing page prototypes using React and JavaScript and had critique sessions with peers which improved code quality",
+                           "Published a blog that got the most hits in one day, in company history"]
     },
 ]
